8420342c7438a5cd222f77d6892feafb2d165908
FathiMalek <fathiabdelmalek0666006253@gmail.com>
2020-12-20 02:43:50 +0100

    fix DB file

--- myportfolio.sql
+++ myportfolio.sql
@@ -7,7 +7,7 @@ CREATE TABLE users(
 	`email`    		VARCHAR(100) 	NOT NULL UNIQUE,
 	`password` 		VARCHAR(400) 	NOT NULL,
 	`fullname` 		VARCHAR(50),
-    `date_joined` 	DATE			NOT NULL DEFAULT(now()),
+    `join_date` 	DATE			NOT NULL DEFAULT(now()),
 	`isadmin`  		ENUM('Y','N')	NOT NULL DEFAULT('N'),
 	CONSTRAINT `id` PRIMARY KEY (`id`)
 );
@@ -17,29 +17,37 @@ INSERT INTO users (`username`, `email`, `password`)
 VALUE ('Youma', 'youma@gmail.com', SHA1('123'));
 INSERT INTO users (`username`, `email`, `password`)
 VALUE ('Chihab', 'chihab@gmail.com', SHA1('123'));
+
 CREATE TABLE categories(
 	`id` 			TINYINT 	 	NOT NULL AUTO_INCREMENT,
 	`title` 		VARCHAR(50)  	NOT NULL UNIQUE,
 	`description` 	VARCHAR(400),
 	`ordering`		INT,
-	`hidden`  		BOOLEAN			NOT NULL DEFAULT(1),
+	`visibility`  	BOOLEAN			NOT NULL DEFAULT(1),
 	`comments`		BOOLEAN			NOT NULL DEFAULT(1),
 	`ads`			BOOLEAN			NOT NULL DEFAULT(1),
 	CONSTRAINT `id` PRIMARY KEY (`id`)
 );
+
 CREATE TABLE `projects`(
 	`id` 			INT 		NOT NULL AUTO_INCREMENT,
 	`name` 			VARCHAR(50) NOT NULL,
 	`description` 	TEXT 		NOT NULL,
-	`add_date` 		DATETIME 	NOT NULL DEFAULT(now()),
-	`image` 		VARCHAR(20),
-    `rating`		TINYINT			NOT NULL DEFAULT(0),
-    `private`		BOOLEAN		NOT NULL DEFAULT(0),
+	`add_date` 		DATE 		NOT NULL DEFAULT(now()),
+	`media` 		VARCHAR(20),
+    `rating`		TINYINT		NOT NULL DEFAULT(0),
+    `visibility`	BOOLEAN		NOT NULL DEFAULT(0),
 	`userID` 		INT,
 	`categoryID`	TINYINT,
 	CONSTRAINT `id` PRIMARY KEY (`id`),
-	CONSTRAINT `user_id` FOREIGN KEY (`userID`) 		REFERENCES `users`(`id`)
+	CONSTRAINT `user_id` 		FOREIGN KEY (`userID`) 		REFERENCES `users`(`id`)
     ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT `category_id` FOREIGN KEY (`categoryID`) 	REFERENCES `categories`(`id`)
+    CONSTRAINT `category_id` 	FOREIGN KEY (`categoryID`) 	REFERENCES `categories`(`id`)
     ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+CREATE OR REPLACE VIEW `projects_view` AS 
+SELECT projects.*, users.username, categories.title AS category_title FROM projects 
+INNER JOIN users ON users.id = projects.userID 
+INNER JOIN categories ON categories.id = projects.categoryID;
+SELECT * FROM projects_view;
